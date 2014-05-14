3cbfeebbaa88fa1d489c93ceeb147bd494bacc79
App Academy Student <student@appacademy.io>
2014-05-14 09:19:52 -0400

    cleaner sliding piece logic, now with working code

--- sliding_piece.rb
+++ sliding_piece.rb
@@ -5,25 +5,25 @@ class SlidingPiece < Piece
   def compute_moves(move)
     moves = []
     current_move = self.position
-    
-    #possible_move = [Vector[move] + Vector[current_move]].map { |a, b| [a.to_i, b.to_i]}
-    possible_move = [move[0] + current_move[0], move[1] + current_move[1]]
-    
-    moves << possible_move
-     if @board.occupied?(possible_move) || moves.count >= 7
-       return moves
+     
+    loop do
+      possible_move = [move[0] + current_move[0], move[1] + current_move[1]]
+      break if possible_move.any? {|el| !(el.between?(0,7))}
+      moves << possible_move
+      break if @board.occupied?(possible_move)
+      current_move = possible_move  
      end
+     
+     moves
   end
   
   def moves
     output = []
-    
-    
-    
-    moves =  self.move_dirs.map { |direction| Piece::DELTAS[direction] }
-    moves.each do |row|
-      row.each do |move|
-        output << compute_moves(move)
+      
+    directions =  self.move_dirs.map { |direction| Piece::DELTAS[direction] }
+    directions.each do |direction|
+      direction.each do |move|
+        output += compute_moves(move)
       end
       
     end
@@ -32,15 +32,3 @@ class SlidingPiece < Piece
     return output.uniq
   end
 end
-
-=begin
-    
-    if self.move_dirs.include?(:horizontal)
-      possible_moves = (-7..7).map {|row| [(cur_row + row), cur_col] }
-      output += possible_moves
-    end
-
-      possible_moves = (-7..7).map {|col| [cur_row, (cur_col + col)] }
-      
-      output += possible_moves
-=end
